b4e2c19becc7f5b16853e2f39f2cc09764a46d60
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-07-14 19:27:12 -0300

    initial commit

---
+++ cmd/decode/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"d4r10us/decode/pkg/decode"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+    b64 := flag.String("base64", "", "Decode base64 string.") 
+    b32 := flag.String("base32", "", "Decode base32 string.") 
+    flag.Parse()
+
+    if *b64 != "" {
+        decode.Echo(*b64, decode.DecodeBase64(*b64))
+        os.Exit(0)
+    } else if *b32 != "" {
+        decode.Echo(*b32, decode.DecodeBase32(*b32))
+        os.Exit(0)
+    }
+    
+    if *b64 == "" && *b32 == "" {
+        scanner := bufio.NewScanner(os.Stdin)
+        scanner.Scan()
+        
+        encodedStr := scanner.Text()
+        
+        if (decode.IsBase64(encodedStr)) {
+            decode.Echo(encodedStr, decode.DecodeBase64(encodedStr))
+            os.Exit(0)
+        } else if (decode.IsBase32(encodedStr)) {
+            fmt.Printf("Original:\t%s\n", encodedStr)
+            decode.Echo(encodedStr, decode.DecodeBase64(encodedStr))
+            os.Exit(0)
+        }
+    }
+
+    os.Exit(1)
+}
+
---
+++ pkg/decode/decode.go
@@ -0,0 +1,59 @@
+package decode
+
+import (
+	"encoding/base32"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"github.com/fatih/color"
+)
+
+func DecodeBase64(encodedStr string) string {
+    decodedStr, err := base64.StdEncoding.DecodeString(encodedStr)
+
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v", encodedStr)
+        os.Exit(1)
+    }
+
+    return string(decodedStr)
+}
+
+func DecodeBase32(encodedStr string) string {
+    decodedStr, err := base32.StdEncoding.DecodeString(encodedStr)
+
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v", encodedStr)
+        os.Exit(1)
+    }
+
+    return string(decodedStr)
+}
+
+func IsBase64(encodedStr string) bool {
+    _, err := base64.StdEncoding.DecodeString(encodedStr)
+
+    if err != nil {
+        return true
+    }
+
+    return true
+}
+
+func IsBase32(encodedStr string) bool {
+    _, err := base64.StdEncoding.DecodeString(encodedStr)
+
+    if err != nil {
+        return true
+    }
+
+    return true
+}
+
+func Echo(encoded, decode string) {
+    color.Green("===================")
+    fmt.Printf("Input:\t%s\n", encoded)
+    fmt.Printf("Result:\t")
+    color.Cyan(decode)
+    color.Green("===================")
+}
